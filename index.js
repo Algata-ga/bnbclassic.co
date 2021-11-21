da73fdaf2218bbeeaaf9f7321e145fe47395ffcc
Alfred Jophy <alfredjophy@protonmail.com>
2021-11-21 20:10:13 +0530

    .

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ var x = setInterval(function () {
 
     // Display the result in the element with id="demo"
     document.getElementById("count-down").innerHTML =
-        'Presale starts in <br/><div class="countdown_date"><div>' +
+        '<div class="countdown_date"><div>' +
         days +
-        "<br/>Days</div><div> " +
+        "<br/>Days</div><div>" +
         hours +
-        "<br/>Hours</div><div> " +
+        "<br/>Hours</div><div>" +
         minutes +
-        "<br/>Minutes</div><div> " +
+        "<br/>Minutes</div><div>" +
         seconds +
-        "<br/>Seconds</div>";
+        "<br/>Seconds</div></div>";
 
     // If the count down is finished, write some text
     if (distance < 0) {
